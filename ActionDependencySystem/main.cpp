1fa0b5ecdebefcf2ce023e640f098f76735c5e51
Peter Mann <peterdmann28@gmail.com>
2018-05-02 08:18:29 +0100

    Updated blurb and todos

--- ActionDependencySystem/main.cpp
+++ ActionDependencySystem/main.cpp
@@ -1,43 +1,6 @@
 /* 
- * This class acts as the base for actions/graphs. In this frameworks, they are actually
- * the same thing, so actions can contain actions. So an action must store all its inputs
- * and outputs. These can be connected to other actions contained within it. Otherwise
- * they can be used to run the action itself.
- *
- * The run method actually executes the actions primary purpose, but the execute method
- * actually executes a the "graph". So this should iterate through the day and call the
- * run method on each action
- *
- * Or have two types, action and actiongraph, both derived from a common type, then
- * actions can be put inside graphs, and graphs can be put inside graphs.
- *
- * Then the graph has the execution functionality on it, and no "run" functionality.
- *
- * Maybe not!
- *
- * Need an action connection to store vertices?
- *
- * Ways to store DAG in code:
- * Store as an adjency list - basically a map of actions to a list of actions that it is connected to (ahead of it)
- * Need to consider how exactly to store connections between multiple inputs and outputs, not traditional dag structure?
- *
- * Iteration
- * Parallel - Need to write a parallelised executor to sort these actions
- * Series - just run a topological sort and iterate that list
- * 
  * Action Registration
  * Should there be some system to register actions? Possibly not, could consider a Houdini style dso registration system?
- * 
- * Features
- * Add timing stats i.e. how long it takes for action to "run", total graph exec time etc etc
- * 
- * Graph
- * Actions are keyed off their names - can only have uniquely named actions/graphs so an unordered_map is fine
- * 
- * Graph
- * Should do a topological sort to get the order in which to execute the actions
- * For a local graph this doesnt matter too much as its all synchronous
- * For parallel graphs we need to determine which actions can be executed in parallel
  * 
  * TODO: Implement serialisation to text file
  *    This requires we have the serialise an action so that it has the * type, * name, * list of inputs, * list of outputs
@@ -51,9 +14,8 @@
  * TODO: Need to add/test functionality to be able to add inputs and outputs to graphs
  * TODO: Test nested graphs
  * TODO: Move the action input validation to the baseaction class. users implement the "action_run" (think of a better name) method but this should never be called directly, should only call "run"
- * TODO: Sort out waiting of the thread pool, so that it waits until all the tasks are finished
  * TODO: Make the deduction of number of threads more clever so that it can optimise how many threads it needs based on how many tasks can execute simultaneously might not need max hardware threads
- * TODO: Make the thread pool return futures when you add_jobs... this should use packaged tasks to get the future back - then we might be able to wait correctly
+ * TODO: Add timing features in the graphs
  */
 
 #include "LocalActionGraph.h"
