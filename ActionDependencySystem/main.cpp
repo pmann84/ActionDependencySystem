6efc4c5a331327d8297e9f2592c43cdf8b33004c
Peter Mann <peterdmann28@gmail.com>
2018-05-03 12:22:43 +0100

    Consolidated tests

--- ActionDependencySystem/main.cpp
+++ ActionDependencySystem/main.cpp
@@ -25,37 +25,19 @@
 #include "Actions/DelayAction.h"
 #include "ThreadPool.h"
 
-void RunSerialisedTestGraphSimple()
+template<class GraphT>
+void run_test_graph_simple()
 {
    // Local graph code 
    const std::string in = "C:\\Users\\peterm\\powershell\\actionTest.ps1";
    const std::string out = "C:\\Users\\peterm\\powershell\\actionScriptToRun.ps1";
    std::shared_ptr<IAction> action = std::make_shared<CopyFileAction>("CopyFileAction_1", in, out);
-   
-   std::shared_ptr<IAction> runScriptAction = std::make_shared<RunPowershellScriptAction>();
-   runScriptAction->name("RunPowershellScriptAction_1");
-   
-   LocalActionGraph graph;
-   graph.name("LocalActionGraph_1");
-   graph.add_action(action);
-   graph.add_action(runScriptAction);
-   
-   graph.add_connection(action, 0, runScriptAction, 0);
-   graph.run();
-}
-
-void RunParallelisedTestGraphSimple()
-{
-   // Parallel graph code 
-   const std::string in = "C:\\Users\\peterm\\powershell\\actionTest.ps1";
-   const std::string out = "C:\\Users\\peterm\\powershell\\actionScriptToRun.ps1";
-   std::shared_ptr<IAction> action = std::make_shared<CopyFileAction>("CopyFileAction_1", in, out);
 
    std::shared_ptr<IAction> runScriptAction = std::make_shared<RunPowershellScriptAction>();
    runScriptAction->name("RunPowershellScriptAction_1");
 
-   ParallelActionGraph graph;
-   graph.name("ParallelActionGraph_1");
+   GraphT graph;
+   graph.name("ActionGraph_1");
    graph.add_action(action);
    graph.add_action(runScriptAction);
 
@@ -63,35 +45,8 @@ void RunParallelisedTestGraphSimple()
    graph.run();
 }
 
-void RunSerialisedTestGraphWithOptionalInputs()
-{
-   // Create actions
-   std::shared_ptr<IAction> action1 = std::make_shared<DelayAction>("Action_1", 1);
-   std::shared_ptr<IAction> action2 = std::make_shared<DelayAction>("Action_2", 5);
-   std::shared_ptr<IAction> action3 = std::make_shared<DelayAction>("Action_3");
-   std::shared_ptr<IAction> action4 = std::make_shared<DelayAction>("Action_4");
-   std::shared_ptr<IAction> action5 = std::make_shared<DelayAction>("Action_5");
-   std::shared_ptr<IAction> action6 = std::make_shared<DelayAction>("Action_6");
-   std::vector<std::shared_ptr<IAction>> actions = { action1, action2, action3, action4, action5, action6 };
-
-   // Initialise the graph
-   LocalActionGraph graph;
-   graph.name("LocalActionGraph_1");
-   graph.add_actions(actions);
-
-   // Setup the connections
-   graph.add_connection(action2, 0, action5, 1);
-   graph.add_connection(action2, 0, action6, 0);
-   graph.add_connection(action1, 0, action3, 0);
-   graph.add_connection(action3, 0, action4, 0);
-   graph.add_connection(action4, 0, action5, 0);
-   graph.add_connection(action6, 0, action4, 1);
-
-   // GO! GO! GO!
-   graph.run();
-}
-
-void RunParallelTestGraphWithOptionalInputs()
+template<class GraphT>
+void run_test_graph_with_optional_inputs()
 {
    // Create actions
    std::shared_ptr<IAction> action1 = std::make_shared<DelayAction>("Action_1", 1);
@@ -102,8 +57,8 @@ void RunParallelTestGraphWithOptionalInputs()
    std::shared_ptr<IAction> action6 = std::make_shared<DelayAction>("Action_6");
 
    // Initialise the graph
-   ParallelActionGraph graph;
-   graph.name("ParallelActionGraph_1");
+   GraphT graph;
+   graph.name("ActionGraph_1");
    graph.add_actions({ action1, action2, action3, action4, action5, action6 });
 
    // Setup the connections
@@ -118,18 +73,19 @@ void RunParallelTestGraphWithOptionalInputs()
    graph.run();
 }
 
-void ThreadPoolNoItemsTest()
+void thread_pool_no_items_test()
 {
+   // This should not take up 100% CPU when this sleeps
    ThreadPool pool;
    std::this_thread::sleep_for(std::chrono::seconds(50));
 }
 
 int main()
 {
-   //RunSerialisedTestGraphSimple();
-   //RunParallelisedTestGraphSimple();
-   //RunSerialisedTestGraphWithOptionalInputs();
-   RunParallelTestGraphWithOptionalInputs();
-   //ThreadPoolNoItemsTest();
+   //run_test_graph_simple<LocalActionGraph>();
+   //run_test_graph_simple<ParallelActionGraph>();
+   //run_test_graph_with_optional_inputs<LocalActionGraph>();
+   //run_test_graph_with_optional_inputs<ParallelActionGraph>();   
+   //thread_pool_no_items_test();
    return 0;
 }
